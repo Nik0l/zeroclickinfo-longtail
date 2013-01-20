6d774561f977967c078867b4cfc0014fc7e89837
Dylan Lloyd <dylan@dylansserver.com>
2013-01-19 21:26:28 -0500

    added perl module for bills longtail

---
+++ lib/DDG/Longtail/Bills.pm
@@ -0,0 +1,13 @@
+package DDG::Longtail::Bills;
+
+use DDG::Longtail;
+
+name 'Bills';
+description 'US Bills';
+source "GovTrack";
+topics 'everyday';
+category 'facts';
+primary_example_queries 'bill general agreement on tariffs and trade';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-longtail/blob/master/lib/DDG/Longtail/Bills/';
+
+1;
