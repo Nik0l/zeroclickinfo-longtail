f33b2a397998d5913cf249b4a6ded0a202d5c6fb
Dylan Lloyd <dylan@dylansserver.com>
2013-01-21 22:22:26 -0500

    wrap data in CDATA elements, check for null values

--- share/longtail/bills/parse.pl
+++ share/longtail/bills/parse.pl
@@ -14,26 +14,27 @@ my $json = JSON->new->allow_nonref->decode($text);
 #print '<?xml version="1.0" encoding="UTF-8"?>' . "\n"
 #    . '<add allowDups="true">' . "\n\n";
 
+
 print "<doc>\n";
-print '<field name="l2_sec">'
+print '<field name="l2_sec"><![CDATA['
     . $json->{official_title}
-    . "</field>\n";
-print '<field name="l3_sec">'
-    . $json->{actions}[scalar @{ $json->{actions} } - 1]{in_committee}
-    . "</field>\n";
-print '<field name="l4_sec">'
+    . "]]></field>\n";
+print '<field name="l3_sec"><![CDATA['
+    . ($json->{actions}[scalar @{ $json->{actions} } - 1]{in_committee} ? $json->{actions}[scalar @{ $json->{actions} } - 1]{in_committee} : "")
+    . "]]></field>\n";
+print '<field name="l4_sec"><![CDATA['
     . (join ", ", @{ $json->{subjects} })
-    . "</field>\n";
-print '<field name="paragraph">'
+    . "]]></field>\n";
+print '<field name="paragraph"><![CDATA['
     . $json->{summary}{text}
-    . "</field>\n";
-print '<field name="source">'
+    . "]]></field>\n";
+print '<field name="source"><![CDATA['
     . 'http://www.govtrack.us/'
-    . "</field>\n";
-print '<field name="title">'
+    . "]]></field>\n";
+print '<field name="title"><![CDATA['
     . "U.S. House Bill #" . $json->{number}
-    . ($json->{short_title} ne "" ? $json->{short_title} : "")
-    . "</field>\n";
+    . ($json->{short_title} ? $json->{short_title} : "")
+    . "]]></field>\n";
 
 print "</doc>\n\n";
 #    . '</add>';
